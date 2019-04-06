dfaae511dd3378656e9be6c68302daf853aea4c9
o-vuong <oscarvuong@gmail.com>
2019-04-06 04:34:27 -0700

    Done.

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,12 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
+
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
 
 module ApisAndFaradayReading
   class Application < Rails::Application
